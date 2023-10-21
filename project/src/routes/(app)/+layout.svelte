8f865557af8f129e53096ea2d207d9cf43f140ae
Toshiven <kevin.abler19@gmail.com>
2023-10-21 21:35:08 +0800

    added resident database table

--- project/src/routes/(app)/+layout.svelte
+++ project/src/routes/(app)/+layout.svelte
@@ -15,6 +15,7 @@
         </div>
         <div class="flex items-center">
             <div class="flex items-center ml-3">
+               
               <div>
                 <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                   <span class="sr-only">Open user menu</span>
@@ -101,13 +102,13 @@
          </a>
        </li>
        <li>
-         <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-green-200">
+         <a href="/resident-db" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-green-200">
            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
              <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
              <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
              <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Z" />
            </svg>
-           <span class="flex-1 ml-3 whitespace-nowrap">Placeholder</span>
+           <span class="flex-1 ml-3 whitespace-nowrap">Resident Database</span>
          </a>
        </li>
      </ul>
