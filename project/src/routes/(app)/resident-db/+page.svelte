32f5c7799aa194f21ecfe6d9294edf0f652e1cf4
Toshiven <kevin.abler19@gmail.com>
2023-10-22 11:14:10 +0800

    idk what i modified here

--- project/src/routes/(app)/resident-db/+page.svelte
+++ project/src/routes/(app)/resident-db/+page.svelte
@@ -12,7 +12,6 @@
             </svg>
         </div>
         <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search Resident Database" required>
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-green-400 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
     </div>
 </form>
 
