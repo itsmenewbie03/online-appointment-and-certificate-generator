a3359f3245128195c7db91876d99866dc45723e8
Toshiven <kevin.abler19@gmail.com>
2023-10-26 07:49:01 +0800

    create account design

--- src/routes/(app)/create/+page.svelte
+++ src/routes/(app)/create/+page.svelte
@@ -1,27 +1,88 @@
-<div
-class="grid place-items-center mt-10 py-[60px]  px-12 bg-white border border-gray-200 rounded-lg shadow"
->
-<div class="">
-    <form>
-        <div class=" gap-2 mb-6 md:grid-cols-2">
-            <div>
-                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 ">First Name</label>
-                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Andrew Kevin" required>
+
+<div class="grid place-items-center mt-10 py-[60px] px-12 bg-white border border-gray-200 rounded-lg shadow">
+    <div class="">
+        <div>
+            <div class="flex items-center mb-4">
+              <input
+                id="user-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500"
+              />
+              <label
+                for="user-checkbox"
+                class="ml-2 text-sm font-medium text-gray-900"
+              >User</label>
             </div>
-            <div>
-                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 ">Last Name</label>
-                <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Abler" required>
+            <div class="flex items-center mb-4">
+              <input
+                id="employee-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+              />
+              <label
+                for="employee-checkbox"
+                class="ml-2 text-sm font-medium text-gray-900"
+              >Employee</label>
             </div>
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
-                <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="JohnDoe@gmail.com" required>
+            <div class="flex items-center mb-4">
+              <input
+                id="admin-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+              />
+              <label
+                for="admin-checkbox"
+                class="ml-2 text-sm font-medium text-gray-900"
+              >Admin</label>
             </div>
+          </div>
+          <form class="grid grid-cols-2 gap-6">
             <div>
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                <input type="text" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Password" required>
-            </div>
-        <button type="submit" class="mt-6 block text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Create</button>
+                <div class="mb-2">
+                  <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="First Name*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Last Name*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Gender*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="birth" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Date of Birth*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="residency" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Period of Residency*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Password*" required />
+                </div>
+              </div>
+              <div>
+                <div class="mb-2">
+                  <input type="text" id="middle_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Middle Name*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="suffix" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Suffix" />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Email*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Address*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="contact" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Phone Number*" required />
+                </div>
+                <div class="mb-2">
+                  <input type="text" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Confirm Password" required />
+                </div>
+              </div>
+              <button type="button" class=" text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-600 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-32 mb-2 ">Cancel</button>
+              <button id="create-account" type="button" class="text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-600 font-medium rounded-full text-sm px-5 py-2.5 text-center ml-16 mb-2 ">Create Account</button>
+          </form>
     </div>
-    </form>
-</div>
-</div>
+  </div>
+  
--- src/routes/(app)/resident-db/+page.svelte
+++ src/routes/(app)/resident-db/+page.svelte
@@ -1,64 +1,89 @@
-
-
-
 <div class="relative py-12 place-items-center">
-    
-<form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative mb-4 mr-[60%]">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
+    <div class="flex justify-between items-center w-full mb-4">
+        <label
+        for="default-search"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >Search</label
+      >
+      <div class="relative mb-4 mr-[60%]">
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
+          <svg
+            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
         </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search Resident Database" required>
-    </div>
-</form>
-
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-50 uppercase bg-green-400 ">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Full Name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Age
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Gender
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Extra Info
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        <span class="sr-only">Edit</span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="bg-white border-b  hover:bg-green-100">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark">
-                        First Name, MI, Last Name
-                    </th>
-                    <td class="px-6 py-4">
-                        21
-                    </td>
-                    <td class="px-6 py-4">
-                        Male
-                    </td>
-                    <td class="px-6 py-4">
-                        Hehe :D
-                    </td>
-                    <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium pr-2 text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
-                    </td>
-                </tr>
-
-            </tbody>
-        </table>
+        <input
+          type="search"
+          id="default-search"
+          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search Resident Database"
+          required
+        />
+      </div>
+    
+      <div class="flex justify-end mb-2">
+        <a href="/create">
+            <button
+            type="button"
+            class="text-white bg-green-500 hover:bg-green-400 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800"
+            >Create New Account</button
+          >
+        </a>
+      </div>
     </div>
 
+
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-50 uppercase bg-green-400">
+        <tr>
+          <th scope="col" class="px-6 py-3"> Full Name </th>
+          <th scope="col" class="px-6 py-3"> Age </th>
+          <th scope="col" class="px-6 py-3"> Gender </th>
+          <th scope="col" class="px-6 py-3"> Extra Info </th>
+          <th scope="col" class="px-6 py-3">
+            <span class="sr-only">Edit</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white border-b hover:bg-green-100">
+          <th
+            scope="row"
+            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark"
+          >
+            First Name, MI, Last Name
+          </th>
+          <td class="px-6 py-4"> 21 </td>
+          <td class="px-6 py-4"> Male </td>
+          <td class="px-6 py-4"> Hehe :D </td>
+          <td class="px-6 py-4 text-right">
+            <a
+              href="#"
+              class="font-medium pr-2 text-blue-600 dark:text-blue-500 hover:underline"
+              >Edit</a
+            >
+            <a
+              href="#"
+              class="font-medium text-red-600 dark:text-red-500 hover:underline"
+              >Delete</a
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
-
