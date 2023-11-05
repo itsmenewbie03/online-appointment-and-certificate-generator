78bbcb95983e51f9e6b29e35a1681c6819cab83d
Toshiven <kevin.abler19@gmail.com>
2023-11-05 22:12:31 +0800

    refined dashboard & added register

---
+++ src/routes/(app)/transactions/+page.svelte
@@ -0,0 +1,53 @@
+<div class="mt-12 relative overflow-x-auto shadow-md sm:rounded-lg">
+  <table class="w-full text-sm text-left text-gray-800 ">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+          <tr>
+              <th scope="col" class="px-6 py-3">
+                  NAME & DOCUMENT
+              </th>
+              <th scope="col" class="px-6 py-3">
+                  APPOINTMENT STATUS
+              </th>
+              <th scope="col" class="px-6 py-3">
+                  PAYMENT STATUS
+              </th>
+              <th scope="col" class="px-6 py-3">
+                  DATE
+              </th>
+              <th scope="col" class="px-6 py-3">
+                ACTIONS
+            </th>
+              <th scope="col" class="px-6 py-3">
+                  <span class="sr-only">Edit</span>
+              </th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr class="bg-white border-b  hover:bg-gray-50 ">
+            <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap ">
+              <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image">
+              <div class="pl-3">
+                  <div class="text-base font-semibold">First_Name Last_Name</div>
+                  <div class="font-normal text-gray-500">Email</div>
+              </div>  
+          </th>
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                  Apple MacBook Pro 17"
+              </th>
+              <td class="px-6 py-4">
+                  Silver ADD A WORKING DROPDOWN HERE
+              </td>
+              <td class="px-6 py-4">
+                  Laptop
+              </td>
+              <td class="px-6 py-4">
+                  $2999
+              </td>
+              <td class="px-6 py-4 text-right">
+                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+              </td>
+          </tr>
+
+      </tbody>
+  </table>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{svelte,html,js,ts}'],
+  content: [
+    './src/**/*.{svelte,html,js,ts}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("tw-elements/dist/plugin.cjs")]
 }
 
