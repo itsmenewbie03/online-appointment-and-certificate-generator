5c88c0d9e145a5335adb05d911edb00906cd81dc
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-11-14 20:09:55 +0800

    integrated google login
    using neovim xD

---
+++ src/routes/(app)/+page.server.js
@@ -0,0 +1,7 @@
+/** @type {import('./$types').PageLoad} */
+export function load({ url }) {
+  const a = url.searchParams.get("a");
+  return {
+    a: a,
+  };
+}
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,7 +2,6 @@
     import { onMount } from "svelte";
     import toast, { Toaster } from "svelte-french-toast";
     import { goto } from "$app/navigation";
-
     let email;
     let password;
     let recaptcha_token;
@@ -49,7 +48,6 @@
             _alert(body.message);
         }
     };
-
     onMount(() => {
         grecaptcha.ready(() => {
             grecaptcha
@@ -109,12 +107,7 @@
     <div
         class=" max-w-sm py-[60px] w-2/3 px-12 bg-white border border-gray-200 rounded-lg shadow"
     >
-        <form
-            class="auth-form"
-            method="post"
-            action="?/OAuth2"
-            on:submit={handle_submit}
-        >
+        <form class="auth-form" method="post" on:submit={handle_submit}>
             <div class="grid mb-8 place-items-center">
                 <img src="/src/lib/assets/BRGY.-BANGCU1D-150x150.png" alt="" />
             </div>
@@ -158,29 +151,30 @@
                 class="text-white bg-green-400 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-green-400 dark:hover:bg-green-500 dark:focus:ring-green-600"
                 >Login</button
             >
-
-            <div class="grid place-items-center">
-                <button
-                    type="submit"
-                    class="btn-auth text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm my-4 px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
-                >
-                    <svg
-                        class="mr-2 -ml-1 w-4 h-4"
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fab"
-                        data-icon="google"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 488 512"
-                        ><path
-                            fill="currentColor"
-                            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                        /></svg
+            <form action="?/OAuth2" method="POST">
+                <div class="grid place-items-center">
+                    <button
+                        type="submit"
+                        class="btn-auth text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm my-4 px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
                     >
-                    Login in with Google
-                </button>
-            </div>
+                        <svg
+                            class="mr-2 -ml-1 w-4 h-4"
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fab"
+                            data-icon="google"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 488 512"
+                            ><path
+                                fill="currentColor"
+                                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                            /></svg
+                        >
+                        Login in with Google
+                    </button>
+                </div>
+            </form>
             <a
                 href="#"
                 class="grid place-items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
