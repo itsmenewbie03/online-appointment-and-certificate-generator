6d8e3e425f4cc50843bcb543026a605df37a3590
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-11-27 14:41:42 +0800

    feat(accounts): integrate  user/employee account create
    This commit implements the needed api call and value binding to make the form send the request to the endpoint
    
    using neovim xD

--- src/routes/(app)/create/+page.svelte
+++ src/routes/(app)/create/+page.svelte
@@ -1,34 +1,70 @@
 <script>
   import toast, { Toaster } from "svelte-french-toast";
+  import Radio from "$components/Radio.svelte";
 
+  let account_type;
+  let email;
+  let info = {};
+  const options = [
+    {
+      value: "user",
+      label: "User",
+    },
+    {
+      value: "employee",
+      label: "Employee",
+    },
+  ];
   let password = "";
+  let confirm_password = "";
   let passwordError = "";
+
+  const handle_submit = async (e) => {
+    e.preventDefault();
+    validatePassword();
+    if (passwordError) {
+      toast.error("Password does not meet the required conditions.");
+      return;
+    }
+    console.log(`Trying to create ${account_type} account`);
+    const endpoint = `https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/${account_type}/register`;
+    const opts = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        info: info,
+      }),
+    };
+    const resp = await fetch(endpoint, opts).then((res) => res.json());
+    console.log(`REGISTER ENDPOINT RESPONSE: ${JSON.stringify(resp)}`);
+  };
 
   function handlePasswordInput(event) {
     password = event.target.value;
     validatePassword();
   }
 
+  // NOTE: this validation does not check if the password contains atleast one symbol!
+  // also this does not check is the password contains atleast one uppercase character
+  // this validation kinda works but i guess password-validator would make it easier xD
   function validatePassword() {
     const minLength = 8;
     const maxLength = 100;
     const digitRegex = /\d/;
 
-    if (password.length < minLength || password.length > maxLength) {
+    if (password !== confirm_password) {
+      passwordError = "Password and confirm password does not match";
+    } else if (password.length < minLength || password.length > maxLength) {
       passwordError = "Password must be minimum 8 and maximum 100 characters.";
     } else if (!digitRegex.test(password)) {
       passwordError = "Password must contain at least one digit.";
     } else {
       passwordError = "";
-    }
-  }
-
-  function handleCreateAccount() {
-    validatePassword();
-
-    if (passwordError) {
-      toast.error("Password does not meet the required conditions.")
-      return;
     }
   }
 </script>
@@ -40,37 +76,21 @@
 >
   <div class="">
     <div>
-      <div class="flex items-center mb-4">
-        <input
-          id="user-checkbox"
-          type="checkbox"
-          value=""
-          class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500"
-        />
-        <label
-          for="user-checkbox"
-          class="ml-2 text-sm font-medium text-gray-900">User</label
-        >
-      </div>
-      <div class="flex items-center mb-4">
-        <input
-          id="employee-checkbox"
-          type="checkbox"
-          value=""
-          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-        />
-        <label
-          for="employee-checkbox"
-          class="ml-2 text-sm font-medium text-gray-900">Employee</label
-        >
-      </div>
+      <!-- NOTE: PLEASE CONFIGURE THE STYLE FOR THIS  -->
+      <Radio
+        {options}
+        fontSize={16}
+        legend="Account Type"
+        bind:userSelected={account_type}
+      />
     </div>
-    <form class="grid grid-cols-2 gap-6">
+    <form class="grid grid-cols-2 gap-6" on:submit={handle_submit}>
       <div>
         <div class="mb-2">
           <input
             type="text"
             id="first_name"
+            bind:value={info.first_name}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="First Name*"
             required
@@ -80,6 +100,7 @@
           <input
             type="text"
             id="last_name"
+            bind:value={info.last_name}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Last Name*"
             required
@@ -87,19 +108,23 @@
         </div>
         <div class="mb-2">
           <select
+            bind:value={info.gender}
             id="genders"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
           >
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-            <option value="O">Others</option>
-            <option value="P">Prefer not to say</option>
+            <option value="" selected>Select gender...</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Others</option>
+            <option value="prefer not to say">Prefer not to say</option>
           </select>
         </div>
         <div class="mb-2">
           <input
-            type="text"
+            type="date"
             id="birth"
+            bind:value={info.date_of_birth}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Date of Birth*"
             required
@@ -109,6 +134,7 @@
           <input
             type="text"
             id="residency"
+            bind:value={info.period_of_residency}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Period of Residency*"
             required
@@ -125,8 +151,8 @@
             required
           />
           {#if passwordError}
-          <p class="text-red-500 text-sm mt-1">{passwordError}</p>
-        {/if}
+            <p class="text-red-500 text-sm mt-1">{passwordError}</p>
+          {/if}
         </div>
       </div>
       <div>
@@ -134,15 +160,16 @@
           <input
             type="text"
             id="middle_name"
+            bind:value={info.middle_name}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-            placeholder="Middle Name*"
-            required
+            placeholder="Middle Name"
           />
         </div>
         <div class="mb-2">
           <input
             type="text"
             id="suffix"
+            bind:value={info.name_suffix}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Suffix"
           />
@@ -151,6 +178,7 @@
           <input
             type="text"
             id="email"
+            bind:value={email}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Email*"
             required
@@ -160,6 +188,7 @@
           <input
             type="text"
             id="address"
+            bind:value={info.address}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Address*"
             required
@@ -169,6 +198,7 @@
           <input
             type="text"
             id="contact"
+            bind:value={info.phone_number}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
             placeholder="Phone Number*"
             required
@@ -176,10 +206,11 @@
         </div>
         <div class="mb-2">
           <input
-            type="text"
+            type="password"
             id="password"
+            bind:value={confirm_password}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-            placeholder="Confirm Password"
+            placeholder="Confirm Password*"
             required
           />
         </div>
@@ -190,9 +221,8 @@
         >Cancel</button
       >
       <button
-        on:click={handleCreateAccount}
         id="create-account"
-        type="button"
+        type="submit"
         class="text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-600 font-medium rounded-full text-sm px-5 py-2.5 text-center ml-16 mb-2"
         >Create Account</button
       >
