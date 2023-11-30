cb7692370648661cd6b53841336b1f1309db126e
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-11-30 22:03:09 +0800

    feat(user): implement api call for create appointment
    This commit implements the necessary api calls along with value binding to allow user to create an appointment
    
    using neovim xD

--- src/routes/(user)/create-appointment/+page.svelte
+++ src/routes/(user)/create-appointment/+page.svelte
@@ -1,3 +1,43 @@
+<script>
+    import { onMount } from 'svelte'
+    let documents = []
+    let date
+    let document_id
+    const handle_submit = async (event) => {
+        event.preventDefault()
+        const endpoint =
+            'https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/transactions/create'
+
+        const opts = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+            body: JSON.stringify({
+                document_id: document_id,
+                date: date,
+            }),
+        }
+        // console.log(`WE ARE SENDING THIS TO BACKEND: ${opts.body}`)
+        const resp = await fetch(endpoint, opts).then((res) => res.json())
+        console.log(`[CREATE_APPOINTMENT_RESP]: ${JSON.stringify(resp)}`)
+    }
+    onMount(async () => {
+        const endpoint =
+            'https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/documents/list'
+        const opts = {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+        }
+        const resp = await fetch(endpoint, opts).then((res) => res.json())
+        documents = resp.data
+        console.log(`WE GOT [DOCUMENT_LIST]: ${JSON.stringify(resp)}`)
+    })
+</script>
+
 <div
     class="grid place-items-center mt-10 py-[60px] px-12 bg-white border border-gray-200 rounded-lg shadow"
 >
@@ -6,72 +46,38 @@
             <div>
                 <div>
                     <select
+                        bind:value={document_id}
                         id="dropdown1"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
                         required
                     >
-                        <option value="" disabled selected hidden
-                            >Dropdown 1*</option
+                        <option value="" disabled selected
+                            >Select document type...</option
                         >
+                        {#each documents as document}
+                            <option value={document._id}>{document.type}</option
+                            >
+                            <!-- content here -->
+                        {/each}
+                        <!-- TODO: load document list from API -->
                     </select>
                 </div>
             </div>
             <div>
                 <div>
-                    <select
-                        id="dropdown2"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+                    <!-- TODO: add style here pls -->
+                    <label for="date">DATE and TIME</label>
+                    <input
+                        type="datetime-local"
+                        name="date"
+                        bind:value={date}
                         required
-                    >
-                        <option value="" disabled selected hidden
-                            >Dropdown 2*</option
-                        >
-                    </select>
+                    />
                 </div>
             </div>
 
             <!-- CALANDER STUFF (do we api or nah) -->
 
-            <div>
-                <div class="mb-2">
-                    <input
-                        type="text"
-                        id="first_name"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                        placeholder="First Name*"
-                        required
-                    />
-                </div>
-                <div class="mb-2">
-                    <input
-                        type="text"
-                        id="last_name"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                        placeholder="Last Name*"
-                        required
-                    />
-                </div>
-            </div>
-            <div>
-                <div class="mb-2">
-                    <input
-                        type="text"
-                        id="first_name"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                        placeholder="First Name*"
-                        required
-                    />
-                </div>
-                <div class="mb-2">
-                    <input
-                        type="text"
-                        id="last_name"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                        placeholder="Last Name*"
-                        required
-                    />
-                </div>
-            </div>
             <div class="col-span-2 flex justify-center">
                 <button
                     type="button"
@@ -82,7 +88,7 @@
                     id="create-account"
                     type="submit"
                     class="text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-600 font-medium rounded-full text-sm px-5 py-2.5 text-center ml-4"
-                    >Create Account</button
+                    >Create Appointment</button
                 >
             </div>
         </form>
