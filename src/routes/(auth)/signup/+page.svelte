4becdc12d975a8705a72b3e304103f293c8e32f0
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-12-15 18:33:29 +0800

    feat(signup): add user signup api call
    This commit implements the necessary api calls to allow a potential user
    to signup for an account.

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,6 +1,9 @@
 <script>
+    let form
+
     let email
     let password
+    let confirm_password
     let first_name
     let middle_name
     let last_name
@@ -9,37 +12,50 @@
     let address
     let period_of_residency
     let phone_number
+
     import toast, { Toaster } from 'svelte-french-toast'
     const handle_submit = async (event) => {
         event.preventDefault()
+        if (confirm_password !== password) {
+            toast.error('Password does not match')
+            return
+        }
 
         const url =
-            'https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/auth/admin/login'
-        console.log(`logging in with ${email}:${password}`)
+            'https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/user/signup'
+
+        const req_body = {
+            email: email,
+            password: password,
+            info: {
+                first_name: first_name,
+                last_name: last_name,
+                middle_name: middle_name,
+                gender: gender,
+                date_of_birth: date_of_birth,
+                period_of_residency: period_of_residency,
+                phone_number: phone_number,
+                address: address,
+            },
+        }
 
         const resp = await fetch(url, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
             },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
+            body: JSON.stringify(req_body),
         }).then(async (res) => {
             return { status: res.status, body: await res.json() }
         })
         const { status, body } = resp
         const _alert = status == 200 ? toast.success : toast.error
 
-
-        if (status == 200) {
-
-            const { access_token, refresh_token } = body
-
-        }
         _alert(body.message)
-
+        if (status === 200) {
+            // clear the form
+            form.reset()
+        }
     }
 </script>
 
@@ -49,12 +65,7 @@
     <div
         class=" max-w-sm py-[60px] w-2/3 px-12 bg-white border border-gray-200 rounded-lg shadow"
     >
-        <form
-            class="auth-form"
-            method="post"
-            action="?/OAuth2"
-            on:submit={handle_submit}
-        >
+        <form class="auth-form" bind:this={form} on:submit={handle_submit}>
             <div class="grid mb-8 place-items-center">
                 <img src="/src/lib/assets/BRGY.-BANGCU1D-150x150.png" alt="" />
             </div>
@@ -62,7 +73,7 @@
             <div class="grid grid-cols-2 gap-6">
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={email}
                         type="email"
                         name="floating_email"
@@ -78,9 +89,9 @@
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={first_name}
-                        type="email"
+                        type="text"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -95,7 +106,7 @@
                 <div class="relative z-0 w-full mb-6 group">
                     <input
                         bind:value={middle_name}
-                        type="email"
+                        type="text"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -109,9 +120,9 @@
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={last_name}
-                        type="email"
+                        type="text"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -124,15 +135,24 @@
                     >
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
-                    <input
-                    required
+                    <select
+                        required
                         bind:value={gender}
-                        type="email"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
                         placeholder=" "
-                    />
+                    >
+                        <option value="" selected disabled
+                            >Select gender...</option
+                        >
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Others</option>
+                        <option value="prefer not to say"
+                            >Prefer not to say</option
+                        >
+                    </select>
                     <label
                         for="floating_email"
                         class="peer-focus:font-medium absolute text-sm text-gray-950 dark:text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-green-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
@@ -141,9 +161,9 @@
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={date_of_birth}
-                        type="email"
+                        type="date"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -157,9 +177,9 @@
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={address}
-                        type="email"
+                        type="text"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -173,9 +193,9 @@
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={period_of_residency}
-                        type="email"
+                        type="text"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -189,9 +209,9 @@
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
                     <input
-                    required
+                        required
                         bind:value={phone_number}
-                        type="email"
+                        type="tel"
                         name="floating_email"
                         id="floating_email"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
@@ -205,7 +225,7 @@
                 </div>
                 <div class="relative z-0 group">
                     <input
-                    required
+                        required
                         bind:value={password}
                         type="password"
                         name="floating_password"
@@ -221,8 +241,8 @@
                 </div>
                 <div class="relative z-0 group">
                     <input
-                    required
-                        bind:value={password}
+                        required
+                        bind:value={confirm_password}
                         type="password"
                         name="confirm_password"
                         id="confirm_password"
