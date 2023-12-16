d2de27617cd5cf9316ec04d2fc573f9039e51dfa
Toshiven <kevin.abler19@gmail.com>
2023-12-16 19:05:00 +0800

    fix(resident-db): Fix route to create

--- src/routes/dashboard/(admin)/admin/create/+page.svelte
+++ src/routes/dashboard/(admin)/admin/create/+page.svelte
@@ -219,7 +219,7 @@
                     />
                 </div>
             </div>
-            <a href="/resident-db" class="text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-600 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
+            <a href="/dashboard/admin/resident-db" class="text-white bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-green-600 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
                 <button>
                     Cancel
                 </button>
--- src/routes/dashboard/(admin)/admin/resident-db/+page.svelte
+++ src/routes/dashboard/(admin)/admin/resident-db/+page.svelte
@@ -89,7 +89,7 @@
             />
         </div>
         <div class="flex justify-end mb-2">
-            <a href="/create">
+            <a href="/dashboard/admin/create">
                 <button
                     type="button"
                     class="text-white bg-green-500 hover:bg-green-400 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800"
