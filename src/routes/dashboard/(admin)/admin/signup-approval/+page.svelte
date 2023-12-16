fec57023f39cf910727f749d7e9bb92480f3b863
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2023-12-17 00:06:53 +0800

    feat(user.pending): add user pending api call
    This commit implements the necessary API calls to display the list of pending users and allow the admin to approve or reject them

--- src/routes/dashboard/(admin)/admin/signup-approval/+page.svelte
+++ src/routes/dashboard/(admin)/admin/signup-approval/+page.svelte
@@ -3,12 +3,12 @@
     import { invalidateAll } from '$app/navigation'
     import toast, { Toaster } from 'svelte-french-toast'
 
-    let info = [];
+    let infos = []
 
     onMount(async () => {
         try {
             const response = await fetch(
-                'API LINK',
+                'https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/user/pending/list',
                 {
                     method: 'GET',
                     headers: {
@@ -21,7 +21,7 @@
 
             if (response.ok) {
                 let json_response = await response.json()
-                info = json_response.data
+                infos = json_response.data
             } else {
                 console.error(
                     'Failed to fetch transaction data:',
@@ -33,8 +33,55 @@
             console.error('Error fetching transaction data:', error.message)
         }
     })
+    const approve = async (id) => {
+        const endpoint = `https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/user/pending/approve`
+        const opts = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+            body: JSON.stringify({
+                id: id,
+            }),
+        }
+        const resp = await fetch(endpoint, opts).then(async (res) => {
+            return { status: res.status, body: await res.json() }
+        })
+        const { status, body } = resp
+        const _alert = status === 200 ? toast.success : toast.error
+        _alert(body.message)
+        if (status === 200) {
+            // TODO: delete the approved entry from the list
+            infos = infos.filter((info) => info._id !== id)
+        }
+    }
+    const reject = async (id) => {
+        const endpoint = `https://appt-cert-gen-api.itsdarkhere4ever.repl.co/api/user/pending/reject`
+        const opts = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+            body: JSON.stringify({
+                id: id,
+            }),
+        }
+        const resp = await fetch(endpoint, opts).then(async (res) => {
+            return { status: res.status, body: await res.json() }
+        })
+        const { status, body } = resp
+        const _alert = status === 200 ? toast.success : toast.error
+        _alert(body.message)
+        if (status === 200) {
+            // TODO: delete the approved entry from the list
+            infos = infos.filter((info) => info._id !== id)
+        }
+    }
 </script>
 
+<Toaster />
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-50 uppercase bg-green-400">
@@ -51,46 +98,52 @@
         <!-- Example tbody 
       Now do the integration magik  -->
 
-            {#each info as tinfo (info._id)}
-                <tr>
-                    <th
-                        scope="row"
-                        class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                        <div class="pl-3">
-                            <div class="text-base font-semibold">
-                                {` ${info.user_data.first_name} ${info.middle_name} ${info.user_data.last_name}`}
-                            </div>
+        {#each infos as info (info._id)}
+            <tr>
+                <th
+                    scope="row"
+                    class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                    <div class="pl-3">
+                        <div class="text-base font-semibold">
+                            <!-- NOTE: print the name properly even when middle_name is missing -->
+                            {` ${info.info[0].first_name} ${
+                                info.info[0].middle_name
+                                    ? info.info[0].middle_name + ' '
+                                    : ' '
+                            }${info.info[0].last_name}`}
                         </div>
-                    </th>
-                    <td class="px-6 py-3">
-                        {info.gender}
-                    </td>
-                    <td class="px-6 py-3">
-                        {info.birthday}
-                    </td>
-                    <td class="px-6 py-3">
-                        {info.address}
-                    </td>
-                    <td class="px-6 py-3">
-                        {info.period_of_residency}
-                    </td>
-                    <td class="px-6 py-3">
-                        {info.contact}
-                    </td>
-                    <a
-                        href="#"
-                        on:click={approve}
-                        class="font-medium pr-2 text-green-600 dark:text-green-400 hover:underline"
-                        >Approve</a
-                    >
-                    <a
+                    </div>
+                </th>
+                <td class="px-6 py-3">
+                    {info.info[0].gender}
+                </td>
+                <td class="px-6 py-3">
+                    {info.info[0].date_of_birth}
+                </td>
+                <td class="px-6 py-3">
+                    {info.info[0].address}
+                </td>
+                <td class="px-6 py-3">
+                    {info.info[0].period_of_residency}
+                </td>
+                <td class="px-6 py-3">
+                    {info.info[0].phone_number}
+                </td>
+                <a
                     href="#"
-                    on:click={reject}
+                    on:click={approve(info._id)}
+                    class="font-medium pr-2 text-green-600 dark:text-green-400 hover:underline"
+                    >Approve</a
+                >
+                <a
+                    href="#"
+                    on:click={reject(info._id)}
                     class="font-medium pr-2 text-red-600 dark:text-red-400 hover:underline"
                     >Reject</a
                 >
-                </tr>
-            {/each}
-        </table>
+            </tr>
+        {/each}
+    </table>
 </div>
+
